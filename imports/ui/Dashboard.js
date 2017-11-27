83a895f5ea4b3824a83ed05b090cc83bfbddd719
Ionela Alexandra <ionelalexandra28@yahoo.com>
2017-11-27 22:34:51 +0100

    Setup state for title/body. Add handle remove method.

--- imports/ui/Dashboard.js
+++ imports/ui/Dashboard.js
@@ -1,17 +1,56 @@
 import React from 'react';
+import {withTracker} from 'meteor/react-meteor-data';
+import createBrowserHistory from 'history/createBrowserHistory';
 
 import PrivateHeader from './PrivateHeader';
 import NoteList from './NoteList';
 import Editor from './Editor';
 
-export default () => {
-  return (
-    <div>
-      <PrivateHeader title="Dashboard"/>
-      <div className="page-content">
-        <NoteList />
-        <Editor/>
+const customHistory = createBrowserHistory();
+
+export class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  redirectIfUnauthenticated() {
+    if(!Meteor.userId()) {
+      customHistory.replace('/');
+    }
+  }
+  componentWillMount() {
+    console.log(customHistory.location.pathname.split('/')[2]);
+    Session.set('selectedNoteId', customHistory.location.pathname.split('/')[2]);
+    this.redirectIfUnauthenticated();
+  }
+  componentWillUpdate() {
+    this.redirectIfUnauthenticated();
+  }
+  render() {
+    return (
+      <div>
+        <PrivateHeader title="Dashboard"/>
+        <div className="page-content">
+          <NoteList />
+          <Editor/>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
+
+export default withTracker((props) => {
+  return {
+    Session: Session,
+    Subscription: Meteor.subscribe('notes', {
+      onReady: () => {
+        // const id = props.match.params.id;
+        // if (!!id) {
+        //     if (!Notes.findOne({ _id: props.match.params.id }))
+        //     { props.history.push('/NotFound'); }
+        //     else
+        //     { Session.set('selectedNoteId', id); }
+        // }
+      }
+    })
+    };
+})(Dashboard);
--- imports/ui/Editor.js
+++ imports/ui/Editor.js
@@ -4,25 +4,50 @@ import {withTracker} from 'meteor/react-meteor-data';
 import {Session} from 'meteor/session';
 import {Notes} from '../api/notes';
 import PropTypes from 'prop-types';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+const customHistory = createBrowserHistory();
 
 export class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      body: ''
+    }
+  }
   handleBodyChange(e) {
-    this.props.call('notes.update', this.props.note._id, {
-      body: e.target.value
-    });
+    const body = e.target.value;
+    this.setState({body});
+    this.props.call('notes.update', this.props.note._id, {body});
   }
   handleTitleChange(e) {
-    this.props.call('notes.update', this.props.note._id, {
-      title: e.target.value
-    });
+    const title = e.target.value;
+    this.setState({title});
+    this.props.call('notes.update', this.props.note._id, {title: e.target.value});
+  }
+  handleRemoval() {
+    this.props.call('notes.remove', this.props.note._id);
+    customHistory.push('/dashboard');
+  }
+  componentDidUpdate(prevProps, prevState) {
+    const currentNoteId = this.props.note ? this.props.note._id : undefined;
+    const prevNoteId = prevProps.note ? prevProps.note._id : undefined;
+
+    if(currentNoteId && currentNoteId !== prevNoteId) {
+      this.setState({
+        title: this.props.note.title,
+        body: this.props.note.body
+      })
+    }
   }
   render() {
     if(this.props.note) {
       return (
         <div>
-          <input value={this.props.note.title} placeholder="Your title here" onChange={this.handleTitleChange.bind(this)}/>
-          <textarea value={this.props.note.body} placeholder="Your note here" onChange={this.handleBodyChange.bind(this)}></textarea>
-          <button>Delete Note</button>
+          <input value={this.state.title} placeholder="Your title here" onChange={this.handleTitleChange.bind(this)}/>
+          <textarea value={this.state.body} placeholder="Your note here" onChange={this.handleBodyChange.bind(this)}></textarea>
+          <button onClick={this.handleRemoval.bind(this)}>Delete Note</button>
         </div>
       );
     } else if(this.props.selectedNoteId) {
@@ -39,7 +64,8 @@ export class Editor extends React.Component {
 
 Editor.propTypes = {
   note: PropTypes.object,
-  selectedNoteId: PropTypes.string
+  selectedNoteId: PropTypes.string,
+  call: PropTypes.func.isRequired
 };
 
 export default withTracker(() => {
